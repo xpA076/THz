fc042f9ea44c596712c22ea1264020a2f5cecbf6
xpA076 <781398368@qq.com>
2018-01-16 22:45:44 +0800

    restore solve method

--- scripts/solve_1.m
+++ scripts/solve_1.m
@@ -11,13 +11,7 @@ k(3)=r0(2);
 for i=4:380
 par={dat_f(i);T(i);0.000000025;1.00027};
 options = optimset('TolFun',1e-16,'TolX',1e-16);
-[r,resnorm,residual(i,1:2),exitflag,output] = ...
-        lsqnonlin(@(r) thin_errorfunction(r,par),r0,[1 -10],[80 80],options);
-r0=r;
-[r,resnorm,residual(i,1:2),exitflag,output] = ...
-        lsqnonlin(@(r) thin_errorfunction(r,par),r0,[1 -10],[80 80],options);
-r0=r;
-[r,resnorm,residual(i,1:2),exitflag,output] = ...
+[r,~,residual(i,1:2),~,~] = ...
         lsqnonlin(@(r) thin_errorfunction(r,par),r0,[1 -10],[80 80],options);
 r0=r;
 n(i)=r(1);

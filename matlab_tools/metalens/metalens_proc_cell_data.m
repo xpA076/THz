cd1c0596c1c8aa7f0fd42c000b3440ac3fe9e221
xpA076 <781398368@qq.com>
2020-11-06 14:44:07 +0800

    edit cell_data

--- matlab_tools/metalens/metalens_proc_cell_data.m
+++ matlab_tools/metalens/metalens_proc_cell_data.m
@@ -1,6 +1,8 @@
 function data = metalens_proc_cell_data(data_in, model, type)
 %_PROC_CELL_DATA Summary of this function goes here
-%   Detailed explanation goes here
+%   sx, sy, slcp, srcp 为 E=E0*exp[i(kz-wt)] 方程下的透射参数
+
+
 
 %% constants
 load_const;
@@ -14,12 +16,13 @@ ex = data_in.ex;
 ey = data_in.ey;
 
 sconst = calc_sconst(n2.^2, area);
-srcp = 2^-0.5*(ex-1i*ey).*sconst;
-slcp = 2^-0.5*(ex+1i*ey).*sconst;
-
 data.sconst = sconst;
-data.sx = sconst*ex;
-data.sy = sconst*ey;
+
+srcp = conj(2^-0.5 * sconst .* (ex-1i*ey));
+slcp = conj(2^-0.5 * sconst .* (ex+1i*ey));
+
+data.sx = conj(sconst .* ex);
+data.sy = conj(sconst .* ey);
 
 data.srcp = srcp;
 data.slcp = slcp;
@@ -29,15 +32,14 @@ data.tlcp = abs(slcp);
 %% phase
 k0 = 2*pi*data.f/c_const;
 k2 = n2.*k0;
-phi0 = k0*h1 + k2*h2;
+phi0 = k0.*h1 + k2.*h2;
 
 
-%% 结构相位信息
-data.prcp = wrap2( -angle(srcp) - phi0, [-pi pi]);
-data.plcp = wrap2( -angle(slcp) - phi0, [-pi pi]);
+%% 结构相位信息 (phase profile)
+data.prcp = wrap2( angle(srcp) - phi0, [-pi pi]);
+data.plcp = wrap2( angle(slcp) - phi0, [-pi pi]);
 data.px = wrap2( -angle(ex) - phi0, [-pi pi]);
 data.py = wrap2( -angle(ey) - phi0, [-pi pi]);
-
 
 end
 
